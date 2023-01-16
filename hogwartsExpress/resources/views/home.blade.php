0fbdc63a815312d2571a1e4ce680b36670d24e3e
Uxoa <palomita@gmail.com>
2023-01-16 01:47:19 +0100

    Fixing position cards

--- hogwartsExpress/resources/views/home.blade.php
+++ hogwartsExpress/resources/views/home.blade.php
@@ -43,51 +43,9 @@
         </button>
     </div>
 
-    <div class="cardPosition">
-        @foreach ($trainRoutes as $trainRoute)
-            <div class="card card mb-4 border-0">
-                <a href="{{ route('showTrainRoute', $trainRoute->id) }}">
-                    <img src="{{ $trainRoute->img }}" class="card-img-top" alt="...">
-                </a>
-                <div class="card-body">
-                    <h2 class="card-title title titleCard">{{ $trainRoute->name }}</h2>
-                    <h5 class="card-title title infoCard">{{ $trainRoute->date }}, {{ $trainRoute->routeLenght }} min.</h5>
-                    <h5 class="card-title infoCard"> <strong>From:</strong> {{ $trainRoute->origin }} ({{ $trainRoute->departureTime }}) <strong>To:</strong> {{ $trainRoute->destination }} ({{ $trainRoute->arrivalTime }})</h5>
-                    <h5 class="card-title title infoCard">{{ $trainRoute->availableSeats }} Places</h5>
-                    <!-- <p class="card-text text-white">{{ $trainRoute->features }}</p> -->
-                    <form action="{{ route('deleteTrainRoute', ['id'=> $trainRoute->id]) }}" method="post" class="formStyle">
-                        <div class="allButtons">
-                            <div class="selectButton">
-                                @if(Auth::check() && Auth::user()->isAdmin)
-                                    <a class="bt-adm m-1 d-flex justify-content-center align-items-center" style="text-decoration:none" href="{{ route('showTrainRoute', $trainRoute->id) }}">Select</a>
-                                @endif
-                            </div>
-                            <div class="editAndDeleteButton">
-                                <div class="editButton">
-                                    @if(Auth::check() && Auth::user()->isAdmin)
-                                        <a class="bt-adm m-1 d-flex justify-content-center align-items-center" style="text-decoration:none"
-                                            href="{{ route('editTrainRoute', ['id' => $trainRoute->id]) }}">Edit</a>
-                                    @endif
-                                </div>
 
-                                @method('delete')
-                                @csrf
-                                    <div class="removeButton">
-                                        @if(Auth::check() && Auth::user()->isAdmin)
-                                            <button type="submit"
-                                                class="bt-adm m-1 d-flex justify-content-center align-items-center mx-auto"
-                                                onclick="return confirm('Are you sure you want to delete this Train Route? {{ $trainRoute->name }} - ID {{ $trainRoute->id }}')">Remove
-                                            </button>
-                                        @endif
-                                    </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        @endforeach
-
-{{-- Paloma markup card scss --}}
+<div class="cardPosition">
+{{-- markup card scss --}}
 @foreach ($trainRoutes as $trainRoute)
 <div class="main-content">
 <div class="ticket">
@@ -117,7 +75,8 @@
     <div class="seat">
     </div>
     <div class="fineprint">
-        {{ $trainRoute->features }}
+        {{ Str::limit($trainRoute->features, 50)}}
+
     </div>
     <div class="snack">
         @if(Auth::check() && Auth::user()->isAdmin)
--- hogwartsExpress/resources/views/layouts/app.blade.php
+++ hogwartsExpress/resources/views/layouts/app.blade.php
@@ -19,8 +19,7 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js', 'resources/css/app.css'])
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </head>
 <body class="bodyApp">
     <div id="app" class="navBarApp">
@@ -83,6 +82,6 @@
         </main>
     </div>
 
-
+    <script src="{{ asset('js/app.js') }}" ></script>
 </body>
 </html>
