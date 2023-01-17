f92020a40918a53fa94a39c9ecdedaaa9f6ddd60
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-01-17 12:04:15 +0100

    update show view

--- hogwartsExpress/resources/views/layouts/app.blade.php
+++ hogwartsExpress/resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/show.css') }}" rel="stylesheet">
 
 
 
